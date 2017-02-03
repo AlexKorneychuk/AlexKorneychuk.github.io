69c797b0505433b349aec00af48ff71ec142e50d
Alex Korniichuk <alexkorneychuk@gmail.com>
2017-02-03 14:11:48 +0200

    9-10 2nd

--- dz 9-10/js/script.js
+++ dz 9-10/js/script.js
@@ -19,62 +19,72 @@ $(document).ready(function () {
     $('select').niceSelect();
 
 });
+
+
+$(document).ready(function () {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_minimal-grey',
+        radioClass: 'iradio_minimal-grey',
+        increaseArea: '20%' // optional
+    });
+});
+
+
 $(document).ready(function () {
     $(".niceCheck").mousedown(
         /* при клике на чекбоксе меняем его вид и значение */
         function () {
 
             changeCheck(jQuery(this));
-            // });
-
-
-            $(".niceCheck").each(
-                /* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
-                function () {
-                    changeCheckStart(jQuery(this));
-                });
         });
 
 
-    function changeCheck(el)
-    /*
-     функция смены вида и значения чекбокса
-     el - span контейнер дял обычного чекбокса
-     input - чекбокс
-     */ {
-        var el = el,
-            input = el.find("input").eq(0);
+    $(".niceCheck").each(
+        /* при загрузке страницы нужно проверить какое значение имеет чекбокс и в соответствии с ним выставить вид */
+        function () {
+            changeCheckStart(jQuery(this));
+        });
+});
+
+
+function changeCheck(el)
+/*
+ функция смены вида и значения чекбокса
+ el - span контейнер дял обычного чекбокса
+ input - чекбокс
+ */ {
+    var el = el,
+        input = el.find("input").eq(0);
+    checkDisabled = el.find("input").attr("disabled");
+
+    if ((!input.attr("checked")) && (checkDisabled != "disabled")) {
+        el.css("background-position", "0 -17px");
+        input.attr("checked", true)
+    } else {
+        if (checkDisabled != "disabled") {
+            el.css("background-position", "0 0");
+            input.attr("checked", false)
+        }
+    }
+    return true;
+}
+
+
+function changeCheckStart(el)
+/*
+ если установлен атрибут checked, меняем вид чекбокса
+ */ {
+    var el = el,
+        input = el.find("input").eq(0),
         checkDisabled = el.find("input").attr("disabled");
 
-        if ((!input.attr("checked")) && (checkDisabled != "disabled")) {
-            el.css("background-position", "0 -17px");
-            input.attr("checked", true)
-        } else {
-            if (checkDisabled != "disabled") {
-                el.css("background-position", "0 0");
-                input.attr("checked", false)
-            }
-        }
-        return true;
+    if (input.attr("checked")) {
+        el.css("background-position", "0 -17px");
     }
 
-
-    function changeCheckStart(el)
-    /*
-     если установлен атрибут checked, меняем вид чекбокса
-     */ {
-        var el = el,
-            input = el.find("input").eq(0),
-            checkDisabled = el.find("input").attr("disabled");
-
-        if (input.attr("checked")) {
-            el.css("background-position", "0 -17px");
-        }
-
-        /* если checkbox disabled - добавляем соотвсмтвующи класс для нужного вида и добавляем атрибут disabled для вложенного chekcbox */
-        if (checkDisabled) {
-            el.addClass("niceCheckDisabled");
-        }
-        return true;
+    /* если checkbox disabled - добавляем соотвсмтвующи класс для нужного вида и добавляем атрибут disabled для вложенного chekcbox */
+    if (checkDisabled) {
+        el.addClass("niceCheckDisabled");
     }
-});
+    return true;
+}
